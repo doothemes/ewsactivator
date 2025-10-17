e232383a7900752682028bb9a14a6e1681885f81
doothemes <emeza@ews.pe>
2025-10-17 10:38:02 -0500

    Asegurando Index

--- 404.php
+++ 404.php
@@ -1,4 +1,9 @@
-<?php
-header('Content-Type: text/plain; charset=utf-8');
-echo "Write-Host 'ERROR 404' -ForegroundColor Red";
-exit;
+<?php 
+
+require_once __DIR__ . '/init.php';
+
+$title = 'Error 404';
+
+require __DIR__ . '/views/header.php'; 
+require __DIR__ . '/views/errors/404.php'; 
+require __DIR__ . '/views/footer.php';
--- index.php
+++ index.php
@@ -1,23 +1,4 @@
 <?php
 
-// Archivo de Inicialización
-require_once __DIR__ . '/init.php';
-// Determinar el archivo de autenticación basado en la acción GET
-$auth_action = trim($_GET['action'] ?? 'login');
-$auth_file = __DIR__ . '/views/auth/' .$auth_action. '.php';
-// Título de la página y clase del body
-$title = 'Autenticación';
-$bodyclass = 'auth '.$auth_action;
-// Redirigir según el estado de autenticación
-if(is_logged_in() === false){
-    require __DIR__ . '/views/header.php';
-    if(file_exists($auth_file)) {
-        require $auth_file;
-    } else {
-        require __DIR__ . '/views/auth/login.php';
-    }
-    require __DIR__ . '/views/footer.php';
-}else{
-    header('Location: /admin/dashboard'); // Redirigir al panel de administración
-    exit;
-}
+// Incluir archivo de autenticación
+include __DIR__ . '/auth.php';
