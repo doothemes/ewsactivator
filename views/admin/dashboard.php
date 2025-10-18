17e8ea17dd25fc96a2ca78de9c5fd8e72ba760e8
doothemes <emeza@ews.pe>
2025-10-17 19:41:51 -0500

    Agregando Admin

--- views/admin/dashboard.php
+++ views/admin/dashboard.php
@@ -29,7 +29,41 @@
 
     <!-- Inside / Content -->
     <div class="inside">
-        <section id="tab-register" class="content">{{register}}</section>
+        <section id="tab-register" class="content">
+            <div class="header">
+                <h2>Registrar Nueva Licencia</h2>
+                <p>Utilice el siguiente formulario para registrar una nueva licencia de producto de Microsoft.</p>
+            </div>
+            <div class="group">
+                <h3 class="title">Datos Personales</h3>
+                <div class="fields">
+                    <fieldset class="input">
+                        <label for="field-name">Nombre</label>
+                        <input type="text" id="field-name" name="name" required>
+                    </fieldset>
+                    <fieldset class="input">
+                        <label for="field-surname">Apellido</label>
+                        <input type="text" id="field-surname" name="surname" required>
+                    </fieldset>
+                </div>
+            </div>
+            <div class="group">
+                <h3 class="title">Detalles de contacto</h3>
+                <div class="fields">
+                    <fieldset class="input">
+                        <label for="field-email">Correo Electrónico</label>
+                        <input type="email" id="field-email" name="email" required>
+                    </fieldset>
+                    <fieldset class="input">
+                        <label for="field-phone">Número de teléfono</label>
+                        <input type="text" id="field-phone" name="phone" required>
+                    </fieldset>
+                </div>
+            </div>
+            <div class="group">
+                <h3 class="title">Detalles de Orden</h3>
+            </div>
+        </section>
         <section id="tab-finder" class="content">{{finder}}</section>
         <section id="tab-settings" class="content">{{settings}}</section>
     </div>
